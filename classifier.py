f5595410eb7dd146d3b9daf13d546ff3bade9f5c
Mark Tyrrell <mt@Srtrue.local>
2023-03-21 21:47:23 +0000

    update model src

--- classifier.py
+++ classifier.py
@@ -12,35 +12,11 @@ from transformers import BertForSequenceClassification
 st.title('Test Classifier')
 st.markdown('NDC Economy Wide')
 
-# model = st.selectbox('Select the model', ('BART', 'T5'))
 
-# if model == 'BART':
-#     _num_beams = 4
-#     _no_repeat_ngram_size = 3
-#     _length_penalty = 1
-#     _min_length = 12
-#     _max_length = 128
-#     _early_stopping = True
-# else:
-#     _num_beams = 4
-#     _no_repeat_ngram_size = 3
-#     _length_penalty = 2
-#     _min_length = 30
-#     _max_length = 200
-#     _early_stopping = True
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-model = BertForSequenceClassification.from_pretrained(os.path.join('ikitracs_economywide'), num_labels=2)
-tokenizer = BertTokenizer.from_pretrained(os.path.join('ikitracs_economywide'), num_labels=2)
-
-# col1, col2, col3 = st.beta_columns(3)
-# _num_beams = col1.number_input("num_beams", value=_num_beams)
-# _no_repeat_ngram_size = col2.number_input("no_repeat_ngram_size", value=_no_repeat_ngram_size)
-# _length_penalty = col3.number_input("length_penalty", value=_length_penalty)
-
-# col1, col2, col3 = st.beta_columns(3)
-# _min_length = col1.number_input("min_length", value=_min_length)
-# _max_length = col2.number_input("max_length", value=_max_length)
-# _early_stopping = col3.number_input("early_stopping", value=_early_stopping)
+tokenizer = AutoTokenizer.from_pretrained("mtyrrell/ikitracs_economywide")
+model = AutoModelForSequenceClassification.from_pretrained("mtyrrell/ikitracs_economywide")
 
 text = st.text_area('Text Input')
 
